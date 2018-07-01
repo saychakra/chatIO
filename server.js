e7cbe6465f31766c0b6204f27cca8f4c37f1bfb3
sayan <saychakra@gmail.com>
2018-07-01 20:48:28 +0530

    implementing users

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express'),
     app = express(),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server);
-
+    usernames = [];
 server.listen(process.env.PORT || 2018);
 console.log('Server Running...');
 app.get('/', function(req, res) {
@@ -12,8 +12,35 @@ app.get('/', function(req, res) {
 io.sockets.on('connection', function(socket) {
     console.log('Socket Connected...');
 
+    socket.on('new user', function(data, callback) {
+        //checking to see if the username already exists
+        if (usernames.indexOf(data) != -1) {
+            callback(false);
+        } else {
+            callback(true);
+            socket.username = data;
+            usernames.push(socket.username);
+
+            updateUsernames();
+        }
+    });
+    //update usernames
+    function updateUsernames() {
+        io.sockets.emit('usernames', usernames);
+    }
+
     //Send the message
     socket.on('send message', function(data){
-        io.sockets.emit('new message', {msg: data});
+        io.sockets.emit('new message', {msg: data, user: socket.username});
+    });
+
+    //Disconnect event
+    socket.on('disconnect', function(data){
+        if (!socket.username) {
+            return;
+        }
+
+        usernames.splice(usernames.indexOf(socket.username), 1);
+        updateUsernames();
     });
 });
